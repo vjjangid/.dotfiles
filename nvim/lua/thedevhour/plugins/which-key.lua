0e45bc4808fa84bb2603933b7868e4179e75efb6
vijay jangid <vjjangid850@gmail.com>
2025-07-23 20:04:23 +0530

    Added telescope group

--- nvim/lua/thedevhour/plugins/which-key.lua
+++ nvim/lua/thedevhour/plugins/which-key.lua
@@ -1,30 +1,31 @@
 return {
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-    keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = false })
-        end,
-        desc = "Buffer Local Keymaps (which-key)",
-      },
-    },
-    config = function()
-      local wk = require("which-key")
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+		config = function()
+			local wk = require("which-key")
 
-      wk.add(
-      {
-        {"<leader>t", group = "tabs"},
-        {"<leader>s", group = "split"},
-        {"<leader>e", group = "File Explorer"},
-      })
-    end
-  }
+			wk.add(
+				{
+					{"<leader>t", group = "tabs"},
+					{"<leader>s", group = "windows split"},
+					{"<leader>e", group = "File Explorer"},
+					{"<leader>f", group = "Telescope" }
+				})
+		end
+	}
 }
