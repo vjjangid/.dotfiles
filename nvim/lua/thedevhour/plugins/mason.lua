8c039a13108450296ef73d43cd5a9a15559c382c
vijay jangid <vjjangid850@gmail.com>
2025-07-23 19:53:19 +0530

    Update mason.lua with new keymaps

--- nvim/lua/thedevhour/plugins/mason.lua
+++ nvim/lua/thedevhour/plugins/mason.lua
@@ -1,15 +1,24 @@
 return {
 	{
 		"mason-org/mason.nvim",
-    	config = function()
-      		require("mason").setup()
-   		end
+		config = function()
+			require("mason").setup()
+		end
 	},
 	{
 		"mason-org/mason-lspconfig.nvim",
 		dependencies = { "mason.nvim" },
 		config = function()
 			require("mason-lspconfig").setup()
+
+
+			vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { desc = "Show diagnostics at cursor" })
+
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+
+			vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
+			vim.keymap.set("v", "<leader>la", vim.lsp.buf.code_action, { desc = "LSP Code Action (Visual)" })
 		end
 	}
 }
