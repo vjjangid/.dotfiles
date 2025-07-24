66e2849df47d21a13e8d001068e45bb80567ff2c
Vijay jangid <vjjangid850@gmail.com>
2025-07-24 20:03:52 +0530

    Bug: Issues fix in nvim
    update neovim (lazyvim, lsp, mason was not workin)
    omnisharp was not working dotnet path was not set

--- nvim/lua/thedevhour/plugins/mason.lua
+++ nvim/lua/thedevhour/plugins/mason.lua
@@ -1,23 +1,36 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
+	"mason-org/mason.nvim",
+	dependencies = {
 		"mason-org/mason-lspconfig.nvim",
-		dependencies = { "mason.nvim" },
-		config = function()
-			require("mason-lspconfig").setup()
-
-			vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { desc = "Show diagnostics at cursor" })
-
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
-
-			vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
-			vim.keymap.set("v", "<leader>la", vim.lsp.buf.code_action, { desc = "LSP Code Action (Visual)" })
-		end,
 	},
+	config = function()
+		local mason = require("mason")
+
+		local mason_lspconfig = require("mason-lspconfig")
+
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"lua_ls",
+				"omnisharp",
+			},
+		})
+
+		print(mason_lspconfig)
+		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+		vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+
+		vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
+		vim.keymap.set("v", "<leader>la", vim.lsp.buf.code_action, { desc = "LSP Code Action (Visual)" })
+	end,
 }
---
+++ nvim_readme.md
@@ -0,0 +1,9 @@
+# **Issue Log for mac nvim setup**
+1. For omnisharp mac issues
+    - check whether dotnet installe and dotnet path is 
+    
+    ```
+    export DOTNET_ROOT=/usr/local/share/dotnet
+
+    export PATH="$DOTNET_ROOT:$PATH"
+    ```
