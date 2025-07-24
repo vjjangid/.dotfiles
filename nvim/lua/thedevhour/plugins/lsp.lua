50de7436db37542171ac9df5ae49677c06f907db
Vijay Jangid <vijay.jangid@siemens.com>
2025-07-24 16:16:58 +0530

    Added symbols for diagnostic

--- nvim/lua/thedevhour/plugins/lsp.lua
+++ nvim/lua/thedevhour/plugins/lsp.lua
@@ -1,6 +1,26 @@
 return {
 	"neovim/nvim-lspconfig",
 	config = function()
-		local lspconfig = require("lspconfig")
+		vim.diagnostic.config({
+			--virtual_lines = true,
+			underline = true,
+			virtual_text = true,
+			float = {
+				border = "rounded",
+				source = true,
+			},
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "󰅚 ",
+					[vim.diagnostic.severity.WARN] = "󰀪 ",
+					[vim.diagnostic.severity.INFO] = "󰋽 ",
+					[vim.diagnostic.severity.HINT] = "󰌶 ",
+				},
+				numhl = {
+					[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+					[vim.diagnostic.severity.WARN] = "WarningMsg",
+				},
+			},
+		})
 	end,
 }
