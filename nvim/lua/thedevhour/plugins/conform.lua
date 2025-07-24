76b47eb7da545ae8bef71c0e304f0bed2b6f1ef4
Vijay Jangid <vijay.jangid@siemens.com>
2025-07-24 15:01:40 +0530

    Added  c# formatter

--- nvim/lua/thedevhour/plugins/conform.lua
+++ nvim/lua/thedevhour/plugins/conform.lua
@@ -6,11 +6,22 @@ return {
 		conform.setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
+				cs = { "csharpier" }, -- Ensure C# files use the 'csharpier' formatter
+			},
+			formatters = {
+				csharpier = {
+					command = "csharpier",
+					args = {
+						"format",
+						"--write-stdout",
+					},
+					to_stdin = true,
+				},
 			},
 			format_on_save = {
 				lsp_fallback = true,
 				async = false,
-				timeout_ms = 1000,
+				timeout_ms = 2000, -- Increased timeout as C# formatting can be slower
 			},
 		})
 	end,
