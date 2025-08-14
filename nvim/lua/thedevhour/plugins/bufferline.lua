b5ba5a7687eba61a18ef5d5733e239686ee230b7
Vijay Jangid <vijay.jangid@siemens.com>
2025-08-14 15:25:48 +0530

    Added offset for file explorer

--- nvim/lua/thedevhour/plugins/bufferline.lua
+++ nvim/lua/thedevhour/plugins/bufferline.lua
@@ -4,10 +4,22 @@ return {
 	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
 		local bufferline = require("bufferline")
-		bufferline.setup()
+		bufferline.setup({
+			options = {
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Explorer",
+						highlight = "Directory",
+						text_align = "left",
+					},
+				},
+				-- You can add more bufferline options here, if desired
+			},
+		})
 		-- Keymaps for buffer navigation
 		vim.keymap.set("n", "H", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
 		vim.keymap.set("n", "L", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
-		vim.keymap.set("n", "tx", "<cmd>bdelete<CR>", { desc = "delete current buffer" })
+		--vim.keymap.set("n", "tx", "<cmd>bdelete<CR>", { desc = "delete current buffer" })
 	end,
 }
