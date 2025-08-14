5b6e8011ac7969b43a93f015c277ccd2d3f78dc7
Vijay Jangid <vijay.jangid@siemens.com>
2025-08-14 15:08:16 +0530

    BUG: Go to next buffer on <leader>tx

--- nvim/lua/thedevhour/core/keymaps.lua
+++ nvim/lua/thedevhour/core/keymaps.lua
@@ -23,3 +23,33 @@ keymap.set("n", "<C-l>", "<C-w>l", { desc = "move to right window" })
 
 keymap.set("i", "<A-CR>", "<Esc>O", { desc = "Insert newline above without leaving insert mode" })
 keymap.set("i", "<S-CR>", "<Esc>o", { desc = "Insert newline below without leaving insert mode" })
+
+vim.keymap.set("n", "<leader>tx", function()
+	-- Get list of listed buffers
+	local buffers = vim.fn.getbufinfo({ buflisted = 1 })
+	local n = #buffers
+	if n == 1 then
+		-- Only one buffer left, delete it and open nvim-tree
+		vim.cmd("bdelete")
+		vim.cmd("NvimTreeOpen")
+	else
+		local current = vim.api.nvim_get_current_buf()
+		-- Find next buffer (in tabline order, wraps around)
+		local next_buf
+		for i, buf in ipairs(buffers) do
+			if buf.bufnr == current then
+				-- if not last buffer, go to next; else go to prev
+				if i < n then
+					next_buf = buffers[i + 1].bufnr
+				else
+					next_buf = buffers[i - 1].bufnr
+				end
+				break
+			end
+		end
+		-- Switch to next/prev buffer
+		vim.api.nvim_set_current_buf(next_buf)
+		-- Then delete the previous buffer
+		vim.cmd("bdelete " .. current)
+	end
+end, { desc = "Delete current buffer and jump to the next/prev buffer or open NvimTree" })
